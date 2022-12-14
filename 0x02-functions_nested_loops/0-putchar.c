6c3bd96576dc1c1b498a0cd5099d4ab443d7c8e5
Bashir Umar <bashirsarki996@gmail.com>
2022-12-14 03:33:14 -0800

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,10 @@
 /**
  *  * _putchar - writes the character c to stdout
  *   * @c: The character to print
- *    *
+ *    * main - Entry point
  *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int main (void)
+ *      * On error, -1 is returned, and errno is set appropriately. */
+int main(void)
 {
 _putchar('_');
 _putchar('p');
