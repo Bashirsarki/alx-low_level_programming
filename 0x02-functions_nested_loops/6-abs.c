27b252bfb30019ff4b1eee08baf61d9aa8ebcc8a
Bashir Umar <bashirsarki996@gmail.com>
2022-12-14 10:19:23 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry Point
+ * Return: 0 or 1 (Success)
+ * @n: single integer input
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		_putchar(n);
+	else
+		_putchar(n);
+
+	return (n);
+}
