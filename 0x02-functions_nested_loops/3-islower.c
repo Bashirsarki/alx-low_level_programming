2a5be0586d49267b899b1f454f965324a1cf3dcf
Bashir Umar <bashirsarki996@gmail.com>
2022-12-14 08:29:28 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * _islower - Entry point
+ * @c: single letter input
  * Return: 0 or 1 (Success)
  */
 int _islower(int c)
 {
-
 	if (c >= 'a' &&  c <= 'z')
+	{
 	
 		return (1);
-	 else
-	
-		return (0);
 
-	
+	}  else
+	{
+		return (0);
+	}
 }
 
