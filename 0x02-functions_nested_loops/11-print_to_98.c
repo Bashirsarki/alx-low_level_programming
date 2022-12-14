7423c46a5d61f689600fac40cfbbe000f035c40b
Bashir Umar <bashirsarki996@gmail.com>
2022-12-14 14:54:35 -0800

    count

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry Point
+ * @n: single int input
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		printf("%d, ", n);
+	}
+}
