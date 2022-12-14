bbb1cffc0397bc1a4082e59177425a3a4fc4469f
Bashir Umar <bashirsarki996@gmail.com>
2022-12-14 11:41:44 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,20 @@
 #include "main.h"
-/* print_last_digit - Entry point 
- * @ab: single integer input
+/**
+ * print_last_digit - Entry Point
+ * @nld: single integer input
  * Return: 0 or 1 (Success)
  */
 int print_last_digit(int nld)
 {
 	int pld;
 
-		pld = (nld % 10);
+	pld = (nld % 10);
 
-			if (pld < 0)
-					{
-								pld = (-1 * pld);
-									}
+	if (pld < 0)
+	{
+	pld = (-1 * pld);
+	}
 
-				_putchar(pld + '0');
-					return (pld);
+	_putchar(pld + '0');
+	return (pld);
 }
