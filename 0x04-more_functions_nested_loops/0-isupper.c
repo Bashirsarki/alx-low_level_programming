59b939a6a0fddab0ee7098ee15e46fb52f8faab6
Bashir Umar <bashirsarki996@gmail.com>
2022-12-15 14:36:51 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_isupper - Entry Point
- *Return: 0 or 1 (Success)
- *@c: single character input
+ * _isupper - Entry Point
+ * @c: Single letter input
+ * Return: 0 or 1 (Success)
  */
 int _isupper(int c)
 {
-	if (c == 'A')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
