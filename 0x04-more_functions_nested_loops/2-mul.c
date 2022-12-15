9790565d7699ac9fcb3c412571a0c5ad50f62e93
Bashir Umar <bashirsarki996@gmail.com>
2022-12-15 15:09:48 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * Return: 0 or 1 (Success)
+ * @a: single letter integer input
+ * @b: single letter integern input
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
