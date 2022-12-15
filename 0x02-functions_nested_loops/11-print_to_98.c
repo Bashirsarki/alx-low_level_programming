16779bc71d0566a1f813c257d4e7a681a5489535
Bashir Umar <bashirsarki996@gmail.com>
2022-12-14 16:02:48 -0800

    add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98 )
+	if (n < 98)
 	{
 	for (; n <= 98; n++)
 	{
@@ -15,16 +15,18 @@ void print_to_98(int n)
 		else
 			printf("%d, ", n);
 	}
-	} else if (n > 98)
+	} 
+	else if (n > 98)
 	{
 		for (; n >= 98 ; n--)
 		{
-			if ( n == 98)
+			if (n == 98)
 				printf("%d", n);
 			else
 				printf("%d, ", n);
 		}
-	} else 
+	}
+       	else 
 	{
 		printf("98");
 	}
