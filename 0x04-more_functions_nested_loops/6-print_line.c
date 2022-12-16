5d14b0d3c79ad142ad1cd766377ff3acc3adebae
Bashir Umar <bashirsarki996@gmail.com>
2022-12-15 16:39:20 -0800

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - Entry Point
+ * @n: single letter input
+ */
+void print_line(int n)
+{
+	for (; n <= 10 ; n++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
