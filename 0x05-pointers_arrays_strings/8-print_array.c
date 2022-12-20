d9e9eff4fbd8ec5c232222deacde08ef814bc1db
Bashir Umar <bashirsarki996@gmail.com>
2022-12-19 18:30:50 -0800

    Print Array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Entry point
+ * @a: Single letter input
+ * @n: Single letter input
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+}
