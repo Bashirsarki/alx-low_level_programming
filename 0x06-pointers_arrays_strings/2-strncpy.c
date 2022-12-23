c6b4c9efcfce86c86de4605245ad12731b4efdd7
Bashir Umar <bashirsarki996@gmail.com>
2022-12-22 17:15:12 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - Entry point of strings
+ * @dest: destination
+ * @src: source
+ * @n: integer input
+ * Return: return dest to function
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *i = dest;
+	int j;
+
+	for (j = 0; j < n && *src != '\0'; j++)
+	{
+		*dest++ = *src++;
+	}
+		*dest++ = '\0';
+		return (i);
+}
