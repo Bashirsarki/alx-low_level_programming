2e29c99f6e8609ab13687fe3ddb0bf18e6a02296
Bashir Umar <bashirsarki996@gmail.com>
2022-12-23 14:45:33 -0800

    corrrection

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a, b;
+	int a = 0, b = 0;
 
 	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1 == *s2)
-		{
-			return (0);
-		}
-		else if (*s1 < *s2)
+		if (*s1 > *s2)
 		{
 			a = *s1 - *s2;
 			return (a);
 		}
-		else
+		else if (*s1 < *s2)
 		{
 			b = *s1 - *s2;
 			return (b);
@@ -28,4 +24,11 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	else if (*s1 == '\0')
+		return (b);
+	else
+		return (a);
+
 }
