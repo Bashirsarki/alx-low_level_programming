6f1975976af6fabebd69e5ab1ca671b79d0235bb
Bashir Umar <bashirsarki996@gmail.com>
2023-01-02 16:07:33 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Entry point for string to be checked
+ * @s: String pointer to be returned
+ * @c: character to be checked
+ * Return: return pointer to s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+				s++;
+	}
+	return (NULL);
+}
